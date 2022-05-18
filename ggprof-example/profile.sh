bcbbb35ccb5f946b7287ba07130d97c60e77b5d5
Ken Domino <kenneth.domino@domemtech.com>
2022-05-18 14:46:28 -0400

    Updates.

--- ggprof-example/profile.sh
+++ ggprof-example/profile.sh
@@ -2,7 +2,7 @@
 
 make
 export GO111MODULE=on
-go run Test.go -file ../examples/t1.txt &
+go run Test.go -file ../examples/t13.txt &
 go tool pprof http://localhost:6060/debug/pprof/profile?seconds=10
 
 echo When you are finished, make sure to kill the '"go"' process.
